29e1a6be3f5da4ebdb0796442eaeed9853f092d1
Deepak Kohli <deepakkohli1810@gmail.com>
2025-08-17 11:29:58 +0530

    Made change to the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css' ;
-import App from './App.jsx' ;
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-const base = "/AmritsarWalkingTour";
-
+const base =
+  process.env.NODE_ENV === "production"
+    ? "/AmritsarWalkingTour"
+    : "";
 createRoot(document.getElementById("root")).render(
+  
   <StrictMode>
     <BrowserRouter basename={base}>
       <App />
