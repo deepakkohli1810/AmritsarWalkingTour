dc754746b928e4dfe457576b3199851acf43b3c2
Deepak Kohli <deepakkohli1810@gmail.com>
2025-08-17 12:30:10 +0530

    made changes in the index.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import {BrowserRouter} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
-const base =
-  process.env.NODE_ENV === "production"
-    ? "/AmritsarWalkingTour"
-    : "";
 createRoot(document.getElementById("root")).render(
-
   <StrictMode>
-    <BrowserRouter basename={base}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
