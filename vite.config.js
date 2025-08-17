d326dbc3d3f8153d5e3fffe4871b2d773da2731b
Deepak Kohli <deepakkohli1810@gmail.com>
2025-08-17 10:21:55 +0530

    added /dist

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  base: "/AmritsarWalkingTour/",
+  base: "/AmritsarWalkingTour", // your repo name for GitHub Pages
+  server: {
+    host: true, // exposes to local network
+    port: 5173, // optional, default is 5173
+  },
 });
